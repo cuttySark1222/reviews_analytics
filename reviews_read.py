23051a20ab32b7bbb7dd4868e27b84cc70d7c129
Larry Zhou <webgrowbig@gmail.com>
2021-10-27 16:53:40 -0400

    Add Python file

---
+++ reviews_read.py
@@ -0,0 +1,14 @@
+data = [] # 创建空 list 来储存每一笔留言
+review_len = 0 # 声明变量，用来计算留言的总长度
+
+with open('reviews.txt', 'r') as f: # 打开留言文件
+	for line in f: # 把每一笔留言分开存储到预设好的 list 里面
+		data.append(line)
+
+for i in data: # 计算留言总长度
+	review_len += len(i)
+
+avg_review_len = review_len / len(data) # 计算留言平均长度
+
+print(f'文档中总共有{len(data)}笔留言。')
+print(f'平均留言长度为：{avg_review_len}')
