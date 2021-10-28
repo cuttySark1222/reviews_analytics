9394dd08844c671f6749a5972f0592c9b651a1dc
Larry Zhou <webgrowbig@gmail.com>
2021-10-28 12:41:41 -0400

    Update Python file

--- reviews_read.py
+++ reviews_read.py
@@ -10,5 +10,12 @@ for i in data: # 计算留言总长度
 
 avg_review_len = review_len / len(data) # 计算留言平均长度
 
+data_less_100 = [d for d in data if len(d) < 100] # 用 List Comprehension 写法
+# for d in data:
+# 	if len(d) < 100:
+# 		data_less_100.append(d)
+
+
 print(f'文档中总共有{len(data)}笔留言。')
 print(f'平均留言长度为：{avg_review_len}')
+print(f'总共有{len(data_less_100)}笔留言长度小于100')
